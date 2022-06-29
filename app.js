47359caefd0ec700a3976aa98ff360089a60c5c2
ann <dorisann.me@gmail.com>
2022-06-29 14:01:21 +0000

    added comments

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const input = document.querySelectorAll("input");
 let html = " ";
 
 async function currency() {
-  const res = await fetch("https://api.exchangerate.host/latest");
-  const data = await res.json();
-  const rates = data.rates;
+  const res = await fetch("https://api.exchangerate.host/latest"); //fetching the api url(this api didn't have any special key)
+  const data = await res.json(); // we want the response in json.
+  const rates = data.rates; // we just want the rates from the data.
   console.log(rates);
-  const arrayKeys = Object.keys(rates);
+  const arrayKeys = Object.keys(rates); // we want the name of the currencies differently too
   console.log(arrayKeys);
 
   arrayKeys.map((item) => {
-    return (html += `<option value=${item}>${item}</option>`);
+    return (html += `<option value=${item}>${item}</option>`); //displaying the countries' currencies on our html.
   });
 
   for (let i = 0; i < select.length; i++) {
@@ -21,7 +21,7 @@ async function currency() {
   function convert(first, second) {
     input[first].value =
       (input[second].value * rates[select[first].value]) /
-      rates[select[second].value];
+      rates[select[second].value]; //this is a helper function for the conversion of the currencies.
   }
 
   input[0].addEventListener("keyup", () => convert(1, 0));
